17383d425217e9b93c5496b4ca0ef85eda62395b
Patricio Vergara <patoluks31@gmail.com>
2026-06-17 19:46:39 -0300

    Eliminar Laboratorios2Variables1

---
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,16 @@
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }
+}
