108cb6c61663c581f4cbe7f129c9e9a784c4cb59
Patricio Vergara <patoluks31@gmail.com>
2026-06-18 05:41:36 -0300

    Realizo tarea de Clase06 de Java

--- Java/Clase06/Leccion1/HolaMundo/src/Clase06.java
+++ Java/Clase06/Leccion1/HolaMundo/src/Clase06.java
@@ -124,5 +124,48 @@ public class Clase06 {
         
         //Clase 6 Tipos Primitivos en Java Parte 3
         
+        // Tipos primitivos booleanos
+        /*
+        var varBool = false;
+        System.out.println("varBool = " + varBool);
+
+        if (varBool) {
+            System.out.println("La bandera es verde");
+        } else {
+            System.out.println("La bandera es roja");
+        }
+        
+        // Algoritmo: ¿Es mayor de edad?
+        var edad = 18; // Literal tener presente la inferencia de tipos
+        //var adulto = edad >= 18; //Esta es una expresion booleana
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } 
+        else {
+            System.out.println("Eres menor de edad");
+        }
+        // Conversión de tipos primitivos
+        var edad = Integer.parseInt("20");
+        System.out.println("edad = " + (edad + 1));
+        var valorPI = Double.parseDouble("3.1416");
+        System.out.println("valorPI = " + valorPI);
+
+        // Pedir un valor
+        var entrada = new Scanner(System.in);
+        System.out.println("Digite su edad:");
+        edad = Integer.parseInt(entrada.nextLine());
+        System.out.println("edad = " + edad);
+        
+        // Conversión de tipos primitivos en Java Parte 2
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+
+        var fraseChar = "programadores".charAt(4);
+        System.out.println("fraseChar = " + fraseChar);
+
+        System.out.println("Digite un caracter: ");
+        fraseChar = entrada.nextLine().charAt(0);
+        System.out.println("fraseChar = " + fraseChar);*/
+
     }
 }
