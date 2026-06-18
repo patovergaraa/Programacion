0ec84aa874175bfbc847ce966d60def5d2fc350c
Patricio Vergara <patoluks31@gmail.com>
2026-06-17 21:50:34 -0300

    Modificacion en HolaMundo

--- Java/Leccion1/HolaMundo/src/HolaMundo.java
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -1,7 +1,8 @@
 
 public class HolaMundo {
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde Java");
+        /*System.out.println("Hola mundo desde Java");
         
         int miVariable = 10;
         System.out.println(miVariable);
@@ -12,5 +13,30 @@ public class HolaMundo {
         System.out.println(miVariableCadena);
         miVariableCadena = "Sigamos creciendo en programación";
         System.out.println(miVariableCadena);
+         */
+
+        //Var - inferencia de tipos en Java
+        var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 - " + miVariableEntera2);
+        System.out.println("miVariableCadena2 - " + miVariableCadena2);
+        //soutv + tab
+        //Para ejecutar Shift + F6 es ka tecka oara mayuscula
+
+        //Reglas para definir una variable en Java
+        var usuario = "Osvaldo";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union); 
+        
+        var a = 8;
+        var b = 4;
+        System.out.println(usuario + (a + b));
+        
+        //Ejercicio: Caracteres Especiales con Java
+        var nombre = "Natalia";
+        System.out.println("\n Nueva linea: \n" + nombre);//Diagonal inversa y letra n
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("\t.:MENÚ:.");
     }
 }
