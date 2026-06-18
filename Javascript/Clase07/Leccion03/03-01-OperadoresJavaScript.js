75650a2aed9d8c400e8a73a4870a0d47b2d87453
Patricio Vergara <patoluks31@gmail.com>
2026-06-18 19:27:06 -0300

    Modifico algunas cosas para ordenar las carpetas

---
+++ Javascript/Clase07/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,18 @@
+//Ejercicio para encontrar números pares e impares
+
+let parImpar = 1;
+if(parImpar % 2 ==0){
+    console.log("Es uin número PAR")
+}
+else{
+    console.log("Es un número IMPAR")
+}
+
+//Ejercicio: es mayor de edad
+let edad = 20; adulto = 18;
+if( edad >= adulto ){
+    console.log("Es una persona adulta");
+}
+else{
+    console.log("Es una persona menor de edad");
+}
