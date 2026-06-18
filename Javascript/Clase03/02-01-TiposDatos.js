9d711bd411f3fbdd5d08d3e4d24e04b15065736a
Patricio Vergara <patoluks31@gmail.com>
2026-06-18 02:59:33 -0300

    Modifico la Clase03

--- Javascript/Clase03/02-01-TiposDatos.js
+++ Javascript/Clase03/02-01-TiposDatos.js
@@ -1,15 +1,20 @@
+// Clase 3 Tipos de Datos Parte 1
+
 // Tipos de Datos de JavaScript
 /*
 La sintaxis en lo que es comentrios
 es muy similar a la de Java
 realmente diriamos que es identica
 */
+
 //Tipo String
 var nombre = 'Patricio'; //Tipo str
 console.log(nombre);
+
 //Tipo numerico
 var numero = 3000; //Tipo numerico
 console.log(numero);
+
 //Tipo Object
 var objeto = {
     nombre : 'Patricio',
