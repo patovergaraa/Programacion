952d42338eae7d65f7699e698ce3033277bc5765
Patricio Vergara <patoluks31@gmail.com>
2026-06-19 06:04:15 -0300

    Creo nuevo archivo del ejercicio de Clase09

---
+++ Javascript/Clase09/Leccion04/Ejercicio-MesesAñoSwitch.js
@@ -0,0 +1,31 @@
+function meses(mes) {
+    switch (mes) {
+        case 1:
+            return "Enero";
+        case 2:
+            return "Febrero";
+        case 3:
+            return "Marzo";
+        case 4:
+            return "Abril";
+        case 5:
+            return "Mayo";
+        case 6:
+            return "Junio";
+        case 7:
+            return "Julio";
+        case 8:
+            return "Agosto";
+        case 9:
+            return "Septiembre";
+        case 10:
+            return "Octubre";
+        case 11:
+            return "Noviembre";
+        case 12:
+            return "Diciembre";
+        default:
+            return "Mes no válido";
+    }
+}
+console.log(meses(8));
